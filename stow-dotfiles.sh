48a557a55f654fbc63d7925dff92bd21557fbeaa
jordansgrant <jordan@mojotech.com>
2019-06-27 14:38:49 -0600

    Add i3 config

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 
+mkdir ~/bin
+mkdir -p ~/.confg/i3
+
 stow -t $HOME git
 stow -t $HOME vim
 stow -t $HOME tmux
 stow -t $HOME zsh
 stow -t "$HOME/bin" bin
+stow -t "$HOMW/.config/i3" i3
 
 ln -f -s "${0:a:h}/.tool-versions" $HOME
