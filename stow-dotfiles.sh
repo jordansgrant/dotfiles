61c5dd1a00b02f9ceda22a9ef8cd32f6e65d4881
jordansgrant <jordan@mojotech.com>
2019-07-04 12:11:21 -0600

    Add Xresources and fix i3 stow command

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
 
-mkdir ~/bin
-mkdir -p ~/.confg/i3
+mkdir -p $HOME/bin
+mkdir -p $HOME/.confg/i3
 
 stow -t $HOME git
 stow -t $HOME vim
 stow -t $HOME tmux
 stow -t $HOME zsh
+stow -t $HOME xorg
 stow -t "$HOME/bin" bin
-stow -t "$HOMW/.config/i3" i3
+stow -t "$HOME/.config/i3" i3
 
 ln -f -s "${0:a:h}/.tool-versions" $HOME
