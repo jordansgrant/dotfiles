787417b0260719182a8a1e1a3a81ecb555df26fe
jordansgrant <jordan@mojotech.com>
2019-07-05 10:14:36 -0600

    Add dunst config

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -1,8 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
 mkdir -p $HOME/bin
+mkdir -p $HOME/.config
 mkdir -p $HOME/.confg/i3
 mkdir -p $HOME/.confg/rat
+mkdir -p $HOME/.confg/dunst
 mkdir -p $HOME/.confg/nvim
 
 stow -t $HOME git
@@ -14,5 +16,6 @@ stow -t $HOME xorg
 stow -t "$HOME/bin" bin
 stow -t "$HOME/.config/i3" i3
 stow -t "$HOME/.config/rat" rat
+stow -t "$HOME/.config/dunst" dunst
 
 ln -f -s "${0:a:h}/.tool-versions" $HOME
